bad091b3bfd5bc2bbb4c7b807dae3f056a7b27e8
yasheymateen <yasheymateen@gmail.com>
2020-04-22 02:24:56 +0000

    modified lockboxes again after syntax error

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,14 +4,14 @@
 
 def canUnlockAll(boxes):
     """ figures out if all boxes can be opened """
-    checked = [False for i in range(len(boxes))]
+    b_len = len(boxes)
+    checked = [False for i in range(b_len)]
     checked[0] = True
-    stack[0]
-
+    stack = [0]
     while len(stack):
         bx = stack.pop(0)
         for b in boxes[bx]:
-            if isinstance(b, int) and b >= 0 and b < len(boxes) and not checked[b]:
+            if isinstance(b, int) and b >= 0 and b < b_len and not checked[b]:
                 checked[b] = True
                 stack.append(b)
     return all(checked)
