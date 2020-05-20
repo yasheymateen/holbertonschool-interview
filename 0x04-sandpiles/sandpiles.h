b01942e498571c85b6072ba292c861a67bf58eaf
yasheymateen <yasheymateen@gmail.com>
2020-05-20 18:32:02 +0000

    modified header file

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,6 +1,8 @@
 #ifndef SANDPILES_H
 #define SANDPILES_H
 
+void tumble_square(int grid[3][3], int row, int col);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void _print_grid(int grid[3][3]);
 
-#endif
+#endif /* SANDPILES_H */
