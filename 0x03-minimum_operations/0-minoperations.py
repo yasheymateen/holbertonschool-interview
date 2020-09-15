c8117911dcb6614abb9fbbac52a60baa8bc5b14c
yasheymateen <yasheymateen@gmail.com>
2020-09-15 07:18:35 +0000

    took out miscellenous comments

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -18,18 +18,3 @@ def minOperations(n):
         else:
             i += 1
     return res
-
-
-"""
-    sumOp = 0
-
-    if n <= 1:
-        return sumOp
-    for i in range(2, int(math.sqrt(n) + 1)):
-        while n % i == 0:
-            sumOp += i
-            n = n // i
-    if n > 1:
-        sumOp += n
-    return sumOp
-"""
