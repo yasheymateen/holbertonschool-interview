b4f7ebab928762a30f0631f45be4b072cdcbbec7
yasheymateen <yasheymateen@gmail.com>
2020-09-22 05:53:55 +0000

    modified 0 sandpiles

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -11,18 +11,18 @@
  */
 static void print_grid(int grid[3][3])
 {
-  int i, j;
+int i, j;
 
-  for (i = 0; i < 3; i++)
-    {
-      for (j = 0; j < 3; j++)
-	{
-	  if (j)
-	    printf(" ");
-	  printf("%d", grid[i][j]);
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+if (j)
+printf(" ");
+printf("%d", grid[i][j]);
 	}
-      printf("\n");
-    }
+printf("\n");
+}
 }
 
 /**
@@ -35,24 +35,24 @@ static void print_grid(int grid[3][3])
  */
 int check(int grid[3][3], int grid2[3][3])
 {
-  int i, j, flag = 1;
+int i, j, flag = 1;
 
-  for (i = 0; i < 3; i++)
-    {
-      for (j = 0; j < 3; j++)
-	{
-	  if (grid[i][j] > 3)
-	    {
-	      flag = 0;
-	      grid2[i][j] = 1;
-	    }
-	  else
-	    {
-	      grid2[i][j] = 0;
-	    }
-	}
-    }
-  return (flag);
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+if (grid[i][j] > 3)
+{
+flag = 0;
+grid2[i][j] = 1;
+}
+else
+{
+grid2[i][j] = 0;
+}
+}
+}
+return (flag);
 }
 
 
@@ -66,26 +66,26 @@ int check(int grid[3][3], int grid2[3][3])
  */
 void toppling(int grid[3][3], int grid2[3][3])
 {
-  int i, j;
+int i, j;
 
-  for (i = 0; i < 3; i++)
-    {
-      for (j = 0; j < 3; j++)
-	{
-	  if (grid[i][j] > 3 && grid2[i][j] == 1)
-	    {
-	      grid[i][j] -= 4;
-	      if (i - 1 >= 0)
-		grid[i - 1][j] += 1;
-	      if (i + 1 <= 2)
-		grid[i + 1][j] += 1;
-	      if (j - 1 >= 0)
-		grid[i][j - 1] += 1;
-	      if (j + 1 <= 2)
-		grid[i][j + 1] += 1;
-	    }
-	}
-    }
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+if (grid[i][j] > 3 && grid2[i][j] == 1)
+{
+grid[i][j] -= 4;
+if (i - 1 >= 0)
+grid[i - 1][j] += 1;
+if (i + 1 <= 2)
+grid[i + 1][j] += 1;
+if (j - 1 >= 0)
+grid[i][j - 1] += 1;
+if (j + 1 <= 2)
+grid[i][j + 1] += 1;
+}
+}
+}
 }
 
 
@@ -100,20 +100,20 @@ void toppling(int grid[3][3], int grid2[3][3])
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-  int i, j;
+int i, j;
 
-  for (i = 0; i < 3; i++)
-    {
-      for (j = 0; j < 3; j++)
-	{
-	  grid1[i][j] += grid2[i][j];
-	}
-    }
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+grid1[i][j] += grid2[i][j];
+}
+}
 
-  while (check(grid1, grid2) == 0)
-    {
-      printf("=\n");
-      print_grid(grid1);
-      toppling(grid1, grid2);
-    }
+while (check(grid1, grid2) == 0)
+{
+printf("=\n");
+print_grid(grid1);
+toppling(grid1, grid2);
+}
 }
