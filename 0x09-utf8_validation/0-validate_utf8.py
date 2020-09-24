707ac40e8c543c6c8b3f1db13b7b8c4b17d0bbf6
yasheymateen <yasheymateen@gmail.com>
2020-09-24 10:07:30 +0000

    modified 0-validate file

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
 """ UTF 8-Validation """
 
-from itertools import takewhile
-
-
-def bit_count(bytes):
-    """ counts bits from encoding """
-
-    for byte in bytes:
-        n = []
-        exp = 1 << 8
-        while exp:
-            exp >>= 1
-            n.append(bool(byte & exp))
-            yield n
-
 
 def validUTF8(data):
     """ determines if given data set represents valid UTF8 encoding """
@@ -37,25 +23,3 @@ def validUTF8(data):
                 return False
         byte_count -= 1
     return byte_count == 0
-
-
-"""
-bits = bit_count(data)
-    for byte in bits:
-        if byte[0] == 0:
-            continue
-        amt = sum(takewhile(bool, byte))
-        if amt <= 1:
-            return False
-        if amt >= 4:
-            return False
-
-        for _ in range(amt - 1):
-            try:
-                byte = next(bits)
-            except StopIteration:
-                return False
-            if byte[0:2] != [1, 0]:
-                return False
-    return True
-"""
